5544a52b56507322189b2a6b45cafd1b0bc9f302
ducdmn <ndminhduc@gmail.com>
2019-08-23 16:15:15 +0700

    Add crud pages

---
+++ aspnetcoreapp/Pages/Movies/Create.cshtml.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using aspnetcoreapp.Data;
+using aspnetcoreapp.Models;
+
+namespace aspnetcoreapp.Pages.Movies
+{
+    public class CreateModel : PageModel
+    {
+        private readonly aspnetcoreapp.Data.RazorPagesMovieContext _context;
+
+        public CreateModel(aspnetcoreapp.Data.RazorPagesMovieContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Movie Movie { get; set; }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Movie.Add(Movie);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
