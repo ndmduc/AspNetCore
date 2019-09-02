896ab007c0e8c6c268aa1cbd67159ed5858c74e6
ndmduc <ndminhduc@gmail.com>
2019-09-02 20:44:40 +0700

    add search

--- aspnetcoreapp/Pages/Movies/Index.cshtml.cs
+++ aspnetcoreapp/Pages/Movies/Index.cshtml.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using aspnetcoreapp.Data;
 using aspnetcoreapp.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace aspnetcoreapp.Pages.Movies
 {
@@ -21,9 +22,37 @@ namespace aspnetcoreapp.Pages.Movies
 
         public IList<Movie> Movie { get;set; }
 
+        [BindProperty(SupportsGet = true)]
+        public string SearchString { get; set; }
+
+        public SelectList Genres { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public string MovieGenre { get; set;}
+
         public async Task OnGetAsync()
         {
-            Movie = await _context.Movie.ToListAsync();
+            // Movie = await _context.Movie.ToListAsync();
+
+            IQueryable<string> genreQuery = from m in _context.Movie
+                                            orderby m.Genre
+                                            select m.Genre;
+
+            var movies = from m in _context.Movie
+                            select m;
+            if (!string.IsNullOrEmpty(this.SearchString))
+            {
+                movies = movies.Where(s => s.Title.Contains(SearchString));
+            }
+
+            if (!string.IsNullOrEmpty(MovieGenre))
+            {
+                movies = movies.Where(x => x.Genre == MovieGenre);
+            }
+
+            Genres = new SelectList(await genreQuery.Distinct().ToListAsync());
+            Movie = await movies.ToListAsync();
         }
+
     }
 }
