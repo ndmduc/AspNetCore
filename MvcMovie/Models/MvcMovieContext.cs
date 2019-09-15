80f88efbf63354efe094fae3b9cc8123d4629c39
ndmduc <ndminhduc@gmail.com>
2019-09-15 11:43:47 +0700

    Add mvc movie

---
+++ MvcMovie/Models/MvcMovieContext.cs
@@ -0,0 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace MvcMovie.Models
+{
+    public class MvcMovieContext : DbContext
+    {
+        public MvcMovieContext(DbContextOptions<MvcMovieContext> options)
+            : base(options)
+        {
+            
+        }
+
+        public DbSet<Movie> Movie {get;set;}
+    }
+}
